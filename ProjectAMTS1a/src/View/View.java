3676b28c9f7f189e1d3e68e779d4dea1e88270d7
Adam S Miller <asmiller0@gmail.com>
2018-11-19 23:13:06 -0500

    ASM added simple GUI

--- ProjectAMTS1a/src/View/View.java
+++ ProjectAMTS1a/src/View/View.java
@@ -5,6 +5,8 @@
  */
 package View;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import javax.swing.*;
 
 /**
@@ -20,14 +22,34 @@ public class View {
         //create JFrame
         //frame1 = new Frame1();
         
-        JFrame newFrame = new JFrame("hello");
+        JFrame newFrame = new JFrame("The New Program");
         newFrame.setSize(700, 600);
+        
+        JPanel panelMain = new JPanel();
+        panelMain.setVisible(true);
+        newFrame.add(panelMain);
+        
+        JPanel panelTop = new JPanel();
+        panelTop.setLayout(new BorderLayout());
+        panelTop.setVisible(true);
+        panelTop.setBackground(Color.white);
+        
         JPanel newPanel = new JPanel();
-        JButton newButton = new JButton("new button");
-        newPanel.add(newButton);
-        newFrame.add(newPanel);
-        newFrame.setVisible(true);
         newPanel.setVisible(true);
+        JButton newButton = new JButton("Access the Application");
+        JButton newButton2 = new JButton("View/Edit Settings");
+        JButton newButton3 = new JButton("About/Readme");
+        newPanel.add(newButton);
+        newPanel.add(newButton2);
+        newPanel.add(newButton3);
+        panelMain.add(newPanel, "South");
+        newFrame.setVisible(true);
+        
+        newPanel.validate();
+        newPanel.repaint();
+        panelMain.validate();
+        panelMain.repaint();
+        
         newFrame.validate();
         newFrame.repaint();
           
